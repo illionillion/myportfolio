1613982946948ed846b5b02273f83efc2fc2f082
illionillion <yuto.0526.sano@gmail.com>
2020-08-04 18:49:31 +0900

    a

--- js/program.js
+++ js/program.js
@@ -7,3 +7,35 @@ btn.onclick=function movebar(){
 
   btn.classList.toggle('btnx');
 }
+
+document.getElementById('no1').onclick=function scrollToTop() {
+  scrollTo(0, 0);
+}
+
+document.getElementById('no2').onclick=function scrollno2(){
+  var element = document.getElementById('intro'); // 移動させたい位置の要素を取得
+    var rect = element.getBoundingClientRect();
+    var position = rect.top;    // 一番上からの位置を取得
+
+  scrollTo(0, position);
+ 
+}
+document.getElementById('no3').onclick=function scrollno2(){
+  var element = document.getElementById('skills'); // 移動させたい位置の要素を取得
+    var rect = element.getBoundingClientRect();
+    var position = rect.top;    // 一番上からの位置を取得
+
+  scrollTo(0, position);
+ 
+}
+document.getElementById('no4').onclick=function scrollno2(){
+  var element = document.getElementById('main'); // 移動させたい位置の要素を取得
+    var rect = element.getBoundingClientRect();
+    var position = rect.top;    // 一番上からの位置を取得
+
+  scrollTo(0, position);
+ 
+}
+
+
+
